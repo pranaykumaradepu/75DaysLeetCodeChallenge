7eaf930a85ee2efe14e5f72badefdd2c2c094ffc
pranay-kumar-adepu <142247764+pranaykumaradepu@users.noreply.github.com>
2026-03-15 10:36:22 +0530

    Time: 35 ms (63.46%), Space: 26.8 MB (90.31%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-
-        full_set = set(range(1,n+1))
-
         actual_set = set(nums)
-
-        return list(full_set - actual_set)
+        n = len(nums)
+        
+        return [i for i in range(1, n + 1) if i not in actual_set]
 
